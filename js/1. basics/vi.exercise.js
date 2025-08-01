c4a797e256ae565b090ee0748fa09b88c3390fc5
Riddhi Suteri <ridsuteri@gmail.com>
2025-08-01 22:05:00 +0530

    add prototype

--- js/1. basics/vi.exercise.js
+++ js/1. basics/vi.exercise.js
@@ -40,4 +40,47 @@ students.forEach((student) => {
   else student.status = "fail";
 });
  
-console.log(students)
+// console.log(students)
+
+
+// problem - 2
+
+// You are given an array of student objects. Each student has the following properties:
+
+// name (string): The student's name
+
+// score (number): The score obtained in the exam (out of 100)
+
+// passed (boolean): Whether the student passed the exam or not (pass mark is 40+)
+
+// Your tasks are to:
+
+// ✅ Use filter() to get an array of all students who passed the exam.
+
+// 📈 Use map() to get an array of strings in the format: "Alice scored 87 marks."
+
+// 🧮 Use reduce() to calculate the average score of all students.
+
+
+const students2 = [
+  { name: "Alice", score: 87, passed: true },
+  { name: "Bob", score: 36, passed: false },
+  { name: "Charlie", score: 52, passed: true },
+  { name: "Daisy", score: 29, passed: false },
+  { name: "Ethan", score: 91, passed: true }
+];
+
+const passed = students2.filter((student)=>{
+  return student.passed
+})
+
+students2.map((student)=>{
+  console.log(`${student.name} scored ${student.score} marks.`)
+})
+
+const average = (students2.reduce((total, student)=>{
+  return total = total+student.score
+},0))/students2.length;
+
+// console.log(passed)
+console.log(average)
---
+++ js/1. basics/viii.oops.js
@@ -0,0 +1,22 @@
+// object in js
+const person = {
+  name,
+  age,
+  gender,
+
+  talk: function () {},
+
+  walk: function () {},
+};
+
+// class in js
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  talk() {}
+
+  walk() {}
+}
